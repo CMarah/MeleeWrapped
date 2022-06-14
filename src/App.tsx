a143ac17b7b78695b95ea2b7ba45cdacd75f4c9a
Carlos <carlos@marah.dev>
2022-06-14 22:04:24 +0200

    Basic layout & folder input

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  useState,
+  useEffect,
+} from 'react';
+import SlpSelector from './components/SlpSelector';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => {
+  const [slp_files, setSlpFiles] = useState<Array<File>>([]);
+
+  useEffect(() => {
+    console.log('NEW SLP FILES', slp_files, slp_files.length, typeof slp_files.length);
+    // TODO process with slippi-js
+  }, [slp_files]);
+
+  return (<div className="App">
+    <header className="App-header">
+      <p style={{fontSize: '2em', margin: '1.5em 0 0.5em 0'}}>Melee Wrapped</p>
+      <p style={{fontSize: '1.2em', margin: '0 0 1em 0'}}>Your Melee year in review</p>
+    </header>
+    <div className="content">
+      <div style={{marginTop: '3em'}}>
+        <span className="font-semibold text-white">
+          Explore your Melee 2022 <br/>#MeleeWrapped
+        </span>
+      </div>
+      {!slp_files.length && (<div style={{marginTop: '2em'}}>
+        <SlpSelector
+          setSlpFiles={setSlpFiles}
+        />
+      </div>)}
+      {slp_files.length !== 0 && (<div>
+        Loading
+      </div>)}
     </div>
-  );
-}
+  </div>);
+};
 
 export default App;
