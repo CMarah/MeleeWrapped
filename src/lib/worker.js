1034847fa41cd286b754658c4f7f3adb064509cf
Carlos <carlos@marah.dev>
2022-11-15 00:57:04 +0100

    Correctly filter out stages

--- src/lib/worker.js
+++ src/lib/worker.js
@@ -14,17 +14,20 @@ const invalidMetadata = metadata => {
   return false;
 };
 
+const VALID_STAGE_IDs = [2, 3, 8, 28, 31, 32];
 const invalidStats = ({ settings, stocks, inputs }) => {
   // TODO que esté TODA la info que usamos, revisar stats
   try {
     if (settings.is_teams) return true;
-    if (settings.stageId > 32) return true;
+    if (!VALID_STAGE_IDs.includes(settings.stageId)) return true;
     if (![2, 8].includes(settings.gameMode)) return true;
+
     // Check if both players had 2+ stocks at the end
     const p0_deaths = stocks.filter(s => s.playerIndex === 0 && s.endFrame).length;
     const p1_deaths = stocks.filter(s => s.playerIndex === 1 && s.endFrame).length;
     if (p0_deaths <= 2 && p1_deaths <= 2) return true;
 
+    // Check if when quitting out, both players where grounded
     const player_0_airborne = inputs[0].airborne;
     const player_1_airborne = inputs[1].airborne;
     if (!player_0_airborne && !player_1_airborne) return true;
