8339b3af5e12cf49ac999b9236c0f70994185d0a
Niisoks <colebrhall@gmail.com>
2023-12-03 20:50:10 +0000

    Changed onClick to onAuxClick

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,11 +10,11 @@ const Footer = () => (
     <div style={{gap: '1em', fontSize: '1.4em'}}>
       <FontAwesomeIcon
         icon={faGithub} style={{marginRight: '0.5em', cursor: 'pointer'}}
-        onClick={()=> window.open("https://github.com/CMarah/MeleeWrapped", "_blank")}
+        onAuxClick={()=> window.open("https://github.com/CMarah/MeleeWrapped", "_blank")}
       />
       <FontAwesomeIcon
         icon={faTwitter} style={{marginLeft: '0.5em', cursor: 'pointer'}}
-        onClick={()=> window.open("https://twitter.com/CarlosMarah", "_blank")}
+        onAuxClick={()=> window.open("https://twitter.com/CarlosMarah", "_blank")}
       />
     </div>
   </div>
